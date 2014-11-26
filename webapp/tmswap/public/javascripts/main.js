15f4990f4d56404b9bee4a567688374f2c9d7e4f
Kamal Hakim <kamalhakim@livenation.com>
2014-11-25 18:52:39 -0700

    Adding js files.

--- webapp/tmswap/public/javascripts/main.js
+++ webapp/tmswap/public/javascripts/main.js
@@ -1,18 +1,12 @@
 $(function(){
-  var source = $("#search-results").html();
-  var dataTemplate = Handlebars.compile(source);
-  $results = $('#results')
-
-  $('#search').on('keyup', function(e){
-  	if(e.keyCode === 13) {
-			var parameters = { search: $(this).val() };
-  		$.get('/searching', parameters, function(data){
-    		if (data instanceof Array) {
-          $results.html(dataTemplate({resultsArray:data}));
-        } else {
-          $results.html(data);
-        };
-  		});
-  	};
-  });
+  
+ $('#reader').html5_qrcode(function(data){
+      // do something when code is read
+ },
+ function(error){
+     //show read errors 
+ }, function(videoError){
+     //the video stream could be opened
+ });
+  
 });
